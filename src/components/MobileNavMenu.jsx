843a850ab3b36d1445e52d8537da1b1282389da4
Liao Zhu <liao.zhu.cs@gmail.com>
2024-08-30 14:41:11 -0500

    .

--- src/components/MobileNavMenu.jsx
+++ src/components/MobileNavMenu.jsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 function MobileNavMenu() {
   return (
     <div>
-      <ul className="absolute top-20 right-0 bg/50 shadow-md rounded-2xl rounded-t-none w-full md:hidden flex flex-col">
-        <Link href="/about">
-          <li className="p-4 hover:bg-zinc-950/50 w-full transition duration-200 cursor-pointer text-end">About Me</li>
+      <ul className="absolute top-20 right-0 shadow-md rounded-2xl rounded-t-none w-full md:hidden flex flex-col h-screen bg-zinc-950/80 items-start text-4xl">
+        <Link href="/about" className="cursor-pointer hover:bg-zinc-900/50 w-full transition duration-200">
+          <li className="p-4">About Me</li>
         </Link>
-        <Link href="/projects">
-          <li className="p-4 hover:bg-zinc-950/50 w-full transition duration-200 cursor-pointer text-end">Projects</li>
+        <Link href="/projects" className="cursor-pointer hover:bg-zinc-900/50 w-full transition duration-200">
+          <li className="p-4">Projects</li>
         </Link>
-        <Link href="/personal">
-          <li className="p-4 hover:bg-zinc-950/50 w-full transition duration-200 cursor-pointer text-end">Personal Life</li>
+        <Link href="/personal" className="hover:bg-zinc-900/50 w-full transition duration-200 cursor-pointer">
+          <li className="p-4">Personal Life</li>
         </Link>
       </ul>
     </div>
